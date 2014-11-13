4320f028df9a052689d30a2dfcaa31444625b1e1
Justin Wong <justinwong12337@gmail.com>
2014-11-12 22:28:39 -0700

    Adding responsive features

--- js/services/gmapsService.js
+++ js/services/gmapsService.js
@@ -7,7 +7,7 @@ define(["servicesFactory"], function(servicesFactory) {
 				var myOptions = {zoom:14,center:new google.maps.LatLng(43.4720646,-80.54294399999998),mapTypeId: google.maps.MapTypeId.ROADMAP};
 				map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
 				marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(43.4720646,-80.54294399999998)});
-				infowindow = new google.maps.InfoWindow({content:"<b>Justin</b><br/>200 University Ave W<br/>N2L 3G1 Waterloo" });
+				infowindow = new google.maps.InfoWindow({content:"<b>Justin | University of Waterloo</b><br/>200 University Ave W<br/>N2L 3G1 Waterloo" });
 				google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);
 				google.maps.event.addDomListener(window, 'load', gmapsService.init_map);
 			}
