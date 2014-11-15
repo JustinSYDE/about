92b6351fd0b47f073122bac2a05c9ef46caeec02
Justin Wong <justinwong12337@gmail.com>
2014-11-15 13:33:48 -0700

    Adding all remaining partials to bboy page

--- js/directives/bboyDirectives.js
+++ js/directives/bboyDirectives.js
@@ -15,4 +15,32 @@ define(['directivesFactory'], function(directivesFactory){
 			templateUrl: "templates/bboyPartials/bboy-banner.html"
 		};
 	});
+
+	directivesFactory.directive('bboyStory', function(){  
+		return{
+			restrict: 'E',
+			templateUrl: "templates/bboyPartials/bboy-story.html"
+		};
+	});
+
+	directivesFactory.directive('bboyCrew', function(){  
+		return{
+			restrict: 'E',
+			templateUrl: "templates/bboyPartials/bboy-crew.html"
+		};
+	});
+
+	directivesFactory.directive('bboyJourney', function(){  
+		return{
+			restrict: 'E',
+			templateUrl: "templates/bboyPartials/bboy-journey.html"
+		};
+	});
+
+	directivesFactory.directive('bboyContact', function(){  
+		return{
+			restrict: 'E',
+			templateUrl: "templates/bboyPartials/bboy-contact.html"
+		};
+	});
 });
