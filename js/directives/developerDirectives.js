eba5da6c9a36a73b826385716f50c210bf7384f9
Justin Wong <justinwong12337@gmail.com>
2016-04-21 19:31:04 -0500

    Adding landing pages

--- js/directives/developerDirectives.js
+++ js/directives/developerDirectives.js
@@ -2,84 +2,91 @@
 
 define(['directivesFactory'], function(directivesFactory){
 
-	directivesFactory.directive('developerNavbar', function(){  
+	directivesFactory.directive('developerNavbar', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/developer-navbar.html"
 		};
 	});
 
-	directivesFactory.directive('developerBanner', function(){  
+	directivesFactory.directive('developerBanner', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/developer-banner.html"
 		};
 	});
 
-	directivesFactory.directive('developerProjects', function(){  
+	directivesFactory.directive('developerProjects', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/developer-projects.html"
 		};
 	});
 
-	directivesFactory.directive('developerProjectDescriptions', function(){  
+	directivesFactory.directive('developerProjectDescriptions', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/developer-project-descriptions.html"
 		};
 	});
 
-	directivesFactory.directive('developerWeatherSwatch', function(){  
+	directivesFactory.directive('developerWeatherSwatch', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/projects/developer-weather-swatch.html"
 		};
 	});
 
-	directivesFactory.directive('developerZoneSix', function(){  
+	directivesFactory.directive('developerZoneSix', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/projects/developer-zone-six.html"
 		};
 	});
 
-	directivesFactory.directive('developerDontRush', function(){  
+	directivesFactory.directive('developerDontRush', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/projects/developer-dont-rush.html"
 		};
 	});
 
-	directivesFactory.directive('developerSmartTrek', function(){  
+	directivesFactory.directive('developerSmartTrek', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/projects/developer-smart-trek.html"
 		};
 	});
 
-	directivesFactory.directive('developerNotebook', function(){  
+	directivesFactory.directive('developerNotebook', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/projects/developer-notebook.html"
 		};
 	});
 
-	directivesFactory.directive('developerContact', function(){  
+	directivesFactory.directive('developerLandingPages', function(){
+		return{
+			restrict: 'E',
+			templateUrl: "templates/developerPartials/projects/developer-landing-pages.html"
+		};
+	});
+
+	directivesFactory.directive('developerContact', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/developer-contact.html"
 		};
 	});
 
-	directivesFactory.directive('developerWorkExperience', function(){  
+	directivesFactory.directive('developerWorkExperience', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/developerPartials/developer-work-experience.html"
 		};
 	});
 
-	directivesFactory.directive('footerJustin', function(){  
+	directivesFactory.directive('footerJustin', function(){
 		return{
 			restrict: 'E',
 			templateUrl: "templates/footer-justin.html"
